0dde2862283b654d95028f0d801f7ca02eaf5bda
Ethan Liu <yiqunl2@uw.edu>
2021-06-10 22:59:18 +0800

    code coverage

--- tests/testthat/test-my_t.R
+++ tests/testthat/test-my_t.R
@@ -2,4 +2,8 @@ test_that("Returns a list", {
   x <- rnorm(10, mean = 0, sd = 1)
   y <- my_t.test(x, "two.sided", 0)
   expect_type(y, "list")
+  y <- my_t.test(x, mu = -1, alternative = "less")
+  expect_type(y, "list")
+  y <- my_t.test(x, mu = -1, alternative = "greater")
+  expect_type(y, "list")
 })
